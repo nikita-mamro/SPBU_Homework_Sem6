21bf18c6835f887ceb3ef2552ebe9aaa5d0914af
Nikita Mamro <43335327+nikita-mamro@users.noreply.github.com>
2021-05-27 19:26:05 +0300

    Telecommunication | HTTP Proxy Server (#6)
    * Add proxy server for http
    
    * Split proxy server logic and startup in different files

---
+++ Telecommunication/Proxy/main.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+
+from Proxy.proxy import ProxyServer
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    try:
+        port = int(input('Enter port for listening: '))
+    except ValueError:
+        logging.ERROR('Unable to process input')
+        sys.exit(0)
+
+    config = {
+        'HOST_NAME': '',
+        'PORT': port,
+        'MAX_CONNECTIONS': 10,
+        'MAX_REQUEST_SIZE': 4096,
+        'TIMEOUT': 5,
+        'DOMAIN_BLACKLIST': ['neverssl.com']
+    }
+
+    server = ProxyServer(config)
+    server.start()
+
+
+if __name__ == '__main__':
+    main()
